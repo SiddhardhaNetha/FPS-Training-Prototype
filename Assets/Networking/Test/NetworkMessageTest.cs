c644ade80daa10971da99e9c7004656129084069
Siddhardh <siddhardhanetha@gmail.com>
2026-08-17 19:52:38 +0530

    feat: add player ID and state networking test

--- Assets/Networking/Test/NetworkMessageTest.cs
+++ Assets/Networking/Test/NetworkMessageTest.cs
@@ -5,6 +5,30 @@ public class NetworkMessageTest : NetworkBehaviour
 {
     private string lastMessage = "No message received.";
 
+    private ulong assignedPlayerId;
+
+    private Vector3 currentPosition;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        assignedPlayerId =
+            NetworkManager.Singleton.LocalClientId;
+
+        currentPosition = transform.position;
+
+        Debug.Log(
+            "NETWORK SPAWNED - My Client ID: " +
+            assignedPlayerId
+        );
+
+        if (IsClient)
+        {
+            RequestPlayerIdServerRpc();
+        }
+    }
+
     private void OnGUI()
     {
         if (!IsSpawned)
@@ -19,41 +43,111 @@ public class NetworkMessageTest : NetworkBehaviour
 
         GUI.Label(
             new Rect(20, 20, 700, 30),
-            "Last Message: " + lastMessage
+            "My Client ID: " +
+            assignedPlayerId
+        );
+
+        GUI.Label(
+            new Rect(20, 60, 700, 30),
+            "Position: " +
+            currentPosition
+        );
+
+        GUI.Label(
+            new Rect(20, 100, 700, 30),
+            "Last Message: " +
+            lastMessage
         );
 
         if (GUI.Button(
-            new Rect(20, 70, 220, 50),
-            "SEND HELLO TO SERVER"))
+            new Rect(20, 150, 240, 50),
+            "SEND POSITION"))
         {
-            SendHelloServerRpc();
+            SendPositionServerRpc(
+                currentPosition
+            );
         }
     }
 
     [Rpc(SendTo.Server)]
-    private void SendHelloServerRpc()
+    private void RequestPlayerIdServerRpc(
+        RpcParams rpcParams = default)
     {
+        ulong clientId =
+            rpcParams.Receive.SenderClientId;
+
         Debug.Log(
-            "SERVER RECEIVED HELLO FROM CLIENT"
+            "SERVER: Client connected with ID: " +
+            clientId
         );
 
-        lastMessage =
-            "Server received: Hello from Client";
-
-        SendHelloClientRpc(
-            "Hello from Server!"
+        SendPlayerIdClientRpc(
+            clientId,
+            RpcTarget.Single(
+                clientId,
+                RpcTargetUse.Temp
+            )
         );
     }
 
-    [Rpc(SendTo.NotServer)]
-    private void SendHelloClientRpc(
-        string message)
+    [Rpc(SendTo.SpecifiedInParams)]
+    private void SendPlayerIdClientRpc(
+        ulong playerId,
+        RpcParams rpcParams = default)
     {
-        Debug.Log(
-            "CLIENT RECEIVED: " + message
-        );
+        assignedPlayerId = playerId;
 
         lastMessage =
-            "Received: " + message;
+            "Server assigned ID: " +
+            playerId;
+
+        Debug.Log(
+            "CLIENT: Server assigned my ID = " +
+            playerId
+        );
+    }
+
+    [Rpc(SendTo.Server)]
+    private void SendPositionServerRpc(
+        Vector3 position,
+        RpcParams rpcParams = default)
+    {
+        ulong clientId =
+            rpcParams.Receive.SenderClientId;
+
+        Debug.Log(
+            "SERVER RECEIVED STATE | " +
+            "Client ID: " +
+            clientId +
+            " | Position: " +
+            position
+        );
+
+        SendStateReceivedClientRpc(
+            clientId,
+            position,
+            RpcTarget.Single(
+                clientId,
+                RpcTargetUse.Temp
+            )
+        );
+    }
+
+    [Rpc(SendTo.SpecifiedInParams)]
+    private void SendStateReceivedClientRpc(
+        ulong clientId,
+        Vector3 position,
+        RpcParams rpcParams = default)
+    {
+        lastMessage =
+            "Server received state for Client " +
+            clientId +
+            " at " +
+            position;
+
+        Debug.Log(
+            "CLIENT: Server confirmed position = " +
+            position
+        );
     }
 }
